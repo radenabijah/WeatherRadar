9db2b7ff683f7eac3f282bd52b1303b9d098e3a4
radenabijah <raden.abijah@gmail.com>
2025-04-09 16:57:44 +0800

    Switch to bcryptjs for Render compatibility

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const UsersModel = require("./models/User");
 const nodemailer = require("nodemailer");
