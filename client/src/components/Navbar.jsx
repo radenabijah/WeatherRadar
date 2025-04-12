af7b8417486d7c31eccca651950bf82562eb3a88
radenabijah <raden.abijah@gmail.com>
2025-04-12 21:31:17 +0800

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import GpsFixedIcon from "@mui/icons-material/GpsFixed";
